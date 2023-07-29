a28cf32850fe0a5b0ff401517ce9fa3937ff60e4
mahendar <mahendarchikkolla@gmail.com>
2023-07-29 10:44:56 +0530

    date changes

--- hello-react/src/App.jsx
+++ hello-react/src/App.jsx
@@ -6,41 +6,40 @@ import TaskCard from "./TaskCard";
 function App() {
   return (
     <>
-      <div className="ml-36 my-5">
-        <h1 className="text-2xl font-bold my-2">Smarter Tasks</h1>
-        <p className="font-bold text-sm">
-          Project: Graduation FInal Year Project (Revamp College Website)
+      <div className="mx-10 my-5">
+        <h1 className="text-2xl font-bold my-2 mx-32">Smarter Tasks</h1>
+        <p className="font-bold text-sm mx-32 mb-3">
+          Project: Graduation Final Year Project (Revamp College Website)
         </p>
-        <span className="text-xl"></span>
-      </div>
-      <div className="flex justify-center">
-        <div className="flex-1 w-36 px-10 border-2 rounded-xl border-grey-900 p-5 ml-32">
-          <h1 className="text-2xl font-bold text-center pb-5">Pending</h1>
-          <TaskCard
-            title="Build the website with static content"
-            dueDate="10th April"
-            assigneeName="Rohit S"
-          />
-          <TaskCard
-            title="Add blog"
-            dueDate="22nd March"
-            assigneeName="Rohit M"
-          />
-          <button className="bg-gray-200 px-5 rounded-md">+ New Task</button>
-        </div>
+        <div className="flex justify-center">
+          <div className="flex-1 w-36 px-10 border-2 rounded-xl border-grey-900 p-5 ml-32">
+            <h1 className="text-2xl font-bold text-center pb-5">Pending</h1>
+            <TaskCard
+              title="Build the website with static content"
+              dueDate="10th April"
+              assigneeName="Rohit S"
+            />
+            <TaskCard
+              title="Add blog"
+              dueDate="22nd March"
+              assigneeName="Rohit M"
+            />
+            <button className="bg-gray-200 px-5 rounded-md">+ New Task</button>
+          </div>
 
-        <div className="flex-1 w-36 px-10 border-2 rounded-xl border-grey-900 p-5 ml-14 mr-32">
-          <h1 className="text-2xl font-bold bold text-center pb-5">Done</h1>
-          <TaskCard
-            title="Design the mockup"
-            completedAtDate="10th April"
-            assigneeName="Rohit M"
-          />
-          <TaskCard
-            title="Get approval from principal"
-            completedAtDate="20th April"
-            assigneeName="Ajay S"
-          />
+          <div className="flex-1 w-36 px-10 border-2 rounded-xl border-grey-900 p-5 ml-14 mr-32">
+            <h1 className="text-2xl font-bold bold text-center pb-5">Done</h1>
+            <TaskCard
+              title="Design the mockup"
+              completedAtDate="10th April"
+              assigneeName="Rohit M"
+            />
+            <TaskCard
+              title="Get approval from principal"
+              completedAtDate="20th April"
+              assigneeName="Ajay S"
+            />
+          </div>
         </div>
       </div>
     </>
