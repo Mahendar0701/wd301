02ff980bf522eb8bf6f4bf959d0abb714733f611
mahendar <mahendarchikkolla@gmail.com>
2023-08-02 23:18:01 +0530

    back to home button

--- smarter-tasks/src/pages/Notfound.tsx
+++ smarter-tasks/src/pages/Notfound.tsx
@@ -5,8 +5,11 @@ const NotfoundPage: React.FC = () => {
     <div>
       <h1>404</h1>
       <h1>Page not found</h1>
-      <button className="px-3 py-1 my-3 bg-gray-200 hover:bg-gray-300 rounded">
-        <a href="/home">Return to homepage</a>
+      <button
+        id="backToHomeButton"
+        className="px-3 py-1 my-3 bg-gray-200 hover:bg-gray-300 rounded"
+      >
+        <a href="/home">Back to home</a>
       </button>
     </div>
   );
