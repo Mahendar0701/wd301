63a9d6857453886e44d0f31f11b139e1d887e582
mahendar <mahendarchikkolla@gmail.com>
2023-08-30 08:57:23 +0530

    added pwa

--- smarter-tasks/dev-dist/sw.js
+++ smarter-tasks/dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-16108a29'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.53p3psac2j8"
+    "revision": "0.pfb2tccfkq"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
